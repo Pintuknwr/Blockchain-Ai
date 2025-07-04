f8579afe4f849484d16b6aaaa90e71584c6d0ac8
Pintuknwr <pintuk.ug22.cs@nitp.ac.in>
2025-07-04 13:20:53 +0530

    Server Setup

---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+
+
+const connectDB = require('./config/db');
+
+const app= require('./app.js');
+
+dotenv.config();
+
+
+
+
+
+
+// Connect DB and start server
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error)=>{
+    console.error("Failed to connect to the database:", error);
+    process.exit(1);
+});
